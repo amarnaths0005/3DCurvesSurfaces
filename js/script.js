d92a4f0fcb1f9df5579f8ca4ca01b7dfd4cc218c
Amarnath S <45934217+amarnaths0005@users.noreply.github.com>
2021-02-05 20:25:07 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,7 @@
 "option strict";
 
-let width;
-let height;
-
 window.onload = init;
 
 function init() {
-  width = window.innerWidth;
-  height = window.innerHeight;
-
-  //window.addEventListener("resize", onResize, false);
-  //onResize();
+  document.getElementById("pcSurface").click();
 }
-
-/*
-function onResize() {
-  //console.log("Resizing");
-  width = window.innerWidth;
-  height = window.innerHeight;
-
-  let topHeight = 0.15 * height;
-  let botHeight = 0.85 * height;
-
-  document.getElementById("bottomPanel").style.height = calc(100vh - 50px);
-
-  //document.getElementById("bottomPanel").style.height = calc(100 % -topHeight);
-  //console.log("BotHeight is " + height + ", " + botHeight);
-  //document.getElementById("topPanel").style.height = topHeight;
-  //document.getElementById("bottomPanel").style.height = botHeight;
-} */
